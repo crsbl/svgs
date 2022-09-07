922afaf9440f88e87e0ccf41c5d3b39faa3e2a0a
cortega <cortega@acciontip.cl>
2022-09-07 10:43:51 -0500

    style: svg

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import  svgs from './svgs'
 
 function App() {
+
+console.log(svgs())
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flexCenter">
+      <div style={{width:'20rem', height:'20rem'}}>
+{ svgs().Corazon }
+      </div>
     </div>
   );
 }
